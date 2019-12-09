3ab4e13dc4fe616214c23ee3f667590a508a3efe
unknown <alxmer111@gmail.com>
2019-12-09 22:23:05 +0400

    first controllers

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.28 on 2019-12-06 20:49:08.
+ * Generated for Laravel 5.7.28 on 2019-12-09 18:11:48.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('rest', 'RestTestController')->names('RestTest');
+Route::resource('post', 'Blog\PostController')->names('PostTest');
